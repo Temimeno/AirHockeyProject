1fae49cf9e51099cffbb098a37128c545ade1f43
Temimeno <kittipitch.mold@mail.kmutt.ac.th>
2024-03-17 16:17:34 +0700

    ADD: Quantum Console and Relay Package

---
+++ Assets/Script/RelayManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+
+public class RelayManager : MonoBehaviour
+{
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            AuthenticationService.Instance.SignedIn += () =>
+            {
+                Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+            };
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+    }
+
+    private async void CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(2);
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log("Join code = " + joinCode);
+        }
+
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
