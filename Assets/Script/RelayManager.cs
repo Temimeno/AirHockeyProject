f5212cdce17a0b5f06aa294744d71138e9d35fc7
Temimeno <kittipitch.mold@mail.kmutt.ac.th>
2024-03-17 16:47:50 +0700

    FEAT: Relay Service (Unfinished)

--- Assets/Script/RelayManager.cs
+++ Assets/Script/RelayManager.cs
@@ -5,6 +5,10 @@ using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
+using QFSW.QC;
+using Unity.Networking.Transport.Relay;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 
 public class RelayManager : MonoBehaviour
 {
@@ -22,6 +26,7 @@ public class RelayManager : MonoBehaviour
         }
     }
 
+    [Command]
     private async void CreateRelay()
     {
         try
@@ -29,6 +34,29 @@ public class RelayManager : MonoBehaviour
             Allocation allocation = await RelayService.Instance.CreateAllocationAsync(2);
             string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
             Debug.Log("Join code = " + joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartHost();
+        }
+
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [Command]
+    private async void JoinRelay(string joinCode)
+    {
+        try
+        {
+            Debug.Log("Joining Relay with " + joinCode);
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartClient();
         }
 
         catch (RelayServiceException e)
